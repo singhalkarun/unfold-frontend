10b88df45b80e047fe79f8c65e360a4b5e5c09e5
Karun Agarwal <singhalkarun@gmail.com>
2022-03-06 21:24:54 +0530

    Set height back to screen

--- components/chat/chat-window.tsx
+++ components/chat/chat-window.tsx
@@ -47,10 +47,10 @@ export const ChatWindow: NextPage<Props> = (props: Props) => {
   return (
     <>
       {props.connectedUser !== null ? (
-        <div className='grid grid-rows-8 h-5/6 w-screen '>
+        <div className='grid grid-rows-8 h-screen w-screen '>
           <div className='row-span-1 grid grid-cols-8'>
             <div className='hidden md:inline md:col-span-1'></div>
-            <div className='col-span-8 md:col-span-6 bg-gradient-to-r from-red-300 to-blue-300 rounded m-4 p-1 flex justify-center items-center'>
+            <div className='col-span-8 md:col-span-6 bg-gradient-to-r from-red-300 to-blue-300 rounded00 m-4 p-1 flex justify-center items-center'>
               <h1 className='text-xl font-bold md:text-3xl font-heading text-center p-2'>
                 Hi, {props.currentUser?.name}. You are connected to{' '}
                 {props.connectedUser?.name}.
